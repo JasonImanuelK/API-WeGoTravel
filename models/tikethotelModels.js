f1968edff941c256bf8b926a8ad0d17b45cef906
NatanaelBilly <if-20011@students.ithb.ac.id>
2023-04-08 14:56:37 +0700

    add model for hotel

---
+++ models/tikethotelModels.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const tiketHotelSchema = new mongoose.Schema({
+    tanggal_pemesanan: {
+        type: Date,
+        required: [true, 'tiket hotel harus memiliki tanggal pemesanan'],
+    },
+    status_pemesanan: {  
+        type: String,
+        required: [true, 'tiket hotel harus memiliki status pemesanan']
+    },
+    nama_pemesan: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki nama pemesanan'],
+    },
+    jenis_kelamin: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki jenis kelamin'],
+    },
+    tanggal_lahir: {
+        type: Date,
+        required: [true, 'tiket hotel harus memiliki tanggal lahir'],
+    },
+    email: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki email'],
+    },
+    nomor_telepon: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki nomor telepon'],
+    },
+    tipe_kamar: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki tipe kamar'],
+    },
+    harga: {
+        type: Number,
+        required: [true, 'tiket hotel harus memiliki harga'],
+    },
+    ukuran: {
+        type: Number,
+        required: [true, 'tiket hotel harus memiliki ukuran'],
+    },
+    lama_menginap: {
+        type: Number,
+        required: [true, 'tiket hotel harus memiliki lama menginap'],
+    },
+    tanggal_menginap: {
+        type: Date,
+        required: [true, 'tiket hotel harus memiliki tanggal menginap'],
+    },
+    id_voucher: {
+        type: String,
+        required: [true, 'tiket hotel harus memiliki id_voucher'],
+    }
+});
+
+const tiketHotel = mongoose.model('tiketHotel', tiketHotelSchema);
+
+module.exports = tiketHotel;
