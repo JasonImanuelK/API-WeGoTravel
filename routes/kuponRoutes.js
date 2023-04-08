11a44cfe3aa4bd602ffd0138a3d80f1c0a1d08eb
HansCool <hansjonathank22@gmail.com>
2023-04-08 15:22:14 +0700

    update_profile

--- routes/kuponRoutes.js
+++ routes/kuponRoutes.js
@@ -16,9 +16,9 @@ router
 .route('/lihatKupon/:id_kupon')
 .get(auth('user'), kuponControllers.getKuponUser)
 
-// router
-// .route('/updateKupon/:id_kupon')
-// .put(auth('user'), kuponControllers.updateKupon)
+router
+.route('/updateKupon/:id_kupon')
+.put(auth('user'), kuponControllers.updateKupon)
 
 router
 .route('/deleteKupon/:id_kupon')
