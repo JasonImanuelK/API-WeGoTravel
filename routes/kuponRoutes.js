2df85049480e8419e0ef137b45aa87bf99e554b0
HansCool <hansjonathank22@gmail.com>
2023-04-08 14:04:58 +0700

    hapus_kupon

--- routes/kuponRoutes.js
+++ routes/kuponRoutes.js
@@ -13,11 +13,15 @@ router
 .get(auth('admin'), kuponControllers.getKuponAdmin)
 
 router
-.route('/lihatKupon')
+.route('/lihatKupon/:id_kupon')
 .get(auth('user'), kuponControllers.getKuponUser)
 
+// router
+// .route('/updateKupon/:id_kupon')
+// .put(auth('user'), kuponControllers.updateKupon)
+
 router
-.route('/updateKupon/:id_kupon')
-.put(auth('user'), kuponControllers.updateKupon)
+.route('/deleteKupon/:id_kupon')
+.delete(auth('admin'), kuponControllers.deleteKupon)
 
 module.exports = router;
